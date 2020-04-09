6d9d49b4b5698ac459742eaf3df9bda5e4399ea2
Jack-Heaton <43050713+Jack-Heaton@users.noreply.github.com>
2020-04-09 10:28:17 -0400

    fixes

--- src/config.js
+++ src/config.js
@@ -13,7 +13,7 @@ const dev = {
       APP_CLIENT_ID: "1hhoqsc91dcfpb07uda9f9d18r",
       IDENTITY_POOL_ID: "us-east-1:822f59e3-0485-43cd-84e8-302ae0808edd"
     }
-  }
+  };
 
   const prod = {
     s3: {
@@ -31,3 +31,14 @@ const dev = {
       IDENTITY_POOL_ID: "us-east-1:8f0cc7f7-57f2-4da2-bdfb-962dee63d975"
     }
   };
+
+  // Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+? prod
+: dev;
+
+export default {
+// Add common config values here
+MAX_ATTACHMENT_SIZE: 5000000,
+...config
+};
